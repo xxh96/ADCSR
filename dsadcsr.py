079e1f6636ee5458ac5f458c7b4f99351d4e764a
xxh96 <36263119+xxh96@users.noreply.github.com>
2019-12-31 16:14:58 +0800

    Update dsadcsr.py

--- dsadcsr.py
+++ dsadcsr.py
@@ -7,9 +7,6 @@ from model import common
 def make_model(args, parent=False):
     return DSADCSR(args)
 
-def abandon():#Set ''SKIP AND TAIL'  trainablr = False 
-    return ['skip','tail']
-
 class Scale(nn.Module):
 
     def __init__(self,init_value=1e-3,requires_grad=True):
